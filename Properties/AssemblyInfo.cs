7ebe4a6224d748c9f12760783d390e2857a4cc6e
todd <williatf@gmail.com>
2023-02-04 11:41:02 -0600

    added new classes to the database

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Trendsetters Dance")]
 [assembly: AssemblyProduct("TSD Comp Tabulator")]
-[assembly: AssemblyCopyright("Copyright © Todd Williams. 2021")]
+[assembly: AssemblyCopyright("Copyright © Todd Williams. 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7")]
-[assembly: AssemblyFileVersion("1.0.7")]
+[assembly: AssemblyVersion("1.0.8")]
+[assembly: AssemblyFileVersion("1.0.8")]
