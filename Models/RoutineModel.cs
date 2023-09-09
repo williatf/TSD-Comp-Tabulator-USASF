0d58449ace53d0e17c9f60e9021bf95df505cef3
todd <williatf@gmail.com>
2023-09-09 16:38:48 -0400

    Updated with USASF scoring elements

--- Models/RoutineModel.cs
+++ Models/RoutineModel.cs
@@ -10,24 +10,39 @@ namespace TSD_Comp_Tabulator.Models
 {
     public class RoutineModel : INotifyPropertyChanged
     {
-        private double _j1choreography;
-        private double _j1technique;
-        private double _j1execution;
-        private double _j1artistry;
-        private double _j1showmanship;
-        private double _j1appearance;
-        private double _j2choreography;
-        private double _j2technique;
-        private double _j2execution;
-        private double _j2artistry;
-        private double _j2showmanship;
-        private double _j2appearance;
-        private double _j3choreography;
-        private double _j3technique;
-        private double _j3execution;
-        private double _j3artistry;
-        private double _j3showmanship;
-        private double _j3appearance;
+        private double _j1communication;
+        private double _j1suitability;
+        private double _j1composition;
+        private double _j1staging;
+        private double _j1difficulty;
+        private double _j1synchronization;
+        private double _j1spacing;
+        private double _j1movement;
+        private double _j1dynamics;
+        private double _j1elements;
+
+        private double _j2communication;
+        private double _j2suitability;
+        private double _j2composition;
+        private double _j2staging;
+        private double _j2difficulty;
+        private double _j2synchronization;
+        private double _j2spacing;
+        private double _j2movement;
+        private double _j2dynamics;
+        private double _j2elements;
+
+        private double _j3communication;
+        private double _j3suitability;
+        private double _j3composition;
+        private double _j3staging;
+        private double _j3difficulty;
+        private double _j3synchronization;
+        private double _j3spacing;
+        private double _j3movement;
+        private double _j3dynamics;
+        private double _j3elements;
+
 
         public RoutineModel()
         {
@@ -35,25 +50,38 @@ namespace TSD_Comp_Tabulator.Models
             {
                 Random r = new Random();
 
-                _j1appearance = r.Next(4, 5);
-                _j1choreography = r.Next(20, 25);
-                _j1artistry = r.Next(7, 10);
-                _j1execution = r.Next(20, 25);
-                _j1showmanship = r.Next(7, 10);
-                _j1technique = r.Next(20, 25);
-                _j2appearance = r.Next(4, 5);
-                _j2choreography = r.Next(20, 25);
-                _j2artistry = r.Next(7, 10);
-                _j2execution = r.Next(20, 25);
-                _j2showmanship = r.Next(7, 10);
-                _j2technique = r.Next(20, 25);
-                _j3appearance = r.Next(4, 5);
-                _j3choreography = r.Next(20, 25);
-                _j3artistry = r.Next(7, 10);
-                _j3execution = r.Next(20, 25);
-                _j3showmanship = r.Next(7, 10);
-                _j3technique = r.Next(20, 25);
+                _j1communication = r.Next(5, 10);
+                _j1suitability = r.Next(5, 10);
+                _j1composition = r.Next(5, 10);
+                _j1staging = r.Next(5, 10);
+                _j1difficulty = r.Next(5, 10);
+                _j1synchronization = r.Next(5, 10);
+                _j1spacing = r.Next(5, 10);
+                _j1movement = r.Next(5, 10);
+                _j1dynamics = r.Next(5, 10);
+                _j1elements = r.Next(5, 10);
 
+                _j2communication = r.Next(5, 10);
+                _j2suitability = r.Next(5, 10);
+                _j2composition = r.Next(5, 10);
+                _j2staging = r.Next(5, 10);
+                _j2difficulty = r.Next(5, 10);
+                _j2synchronization = r.Next(5, 10);
+                _j2spacing = r.Next(5, 10);
+                _j2movement = r.Next(5, 10);
+                _j2dynamics = r.Next(5, 10);
+                _j2elements = r.Next(5, 10);
+
+                _j3communication = r.Next(5, 10);
+                _j3suitability = r.Next(5, 10);
+                _j3composition = r.Next(5, 10);
+                _j3staging = r.Next(5, 10);
+                _j3difficulty = r.Next(5, 10);
+                _j3synchronization = r.Next(5, 10);
+                _j3spacing = r.Next(5, 10);
+                _j3movement = r.Next(5, 10);
+                _j3dynamics = r.Next(5, 10);
+                _j3elements = r.Next(5, 10);
             }
         }
 
@@ -71,277 +99,470 @@ namespace TSD_Comp_Tabulator.Models
         public string StudioName { get; set; }
         public string RoutineTitle { get; set; }
 
+
+/*        
+        private double _j1communication;
+        private double _j1suitability;
+        private double _j1composition;
+        private double _j1staging;
+        private double _j1difficulty;
+        private double _j1synchronization;
+        private double _j1spacing;
+        private double _j1movement;
+        private double _j1dynamics;
+        private double _j1elements;
+*/
         // Judge 1 Scores
-        public double J1Choreography
+        public double J1Communication
         {
             get
             {
-                return this._j1choreography;
+                return this._j1communication;
             }
             set
             {
-                if (value != this._j1choreography)
+                if (value != this._j1communication)
                 {
-                    this._j1choreography = value;
+                    this._j1communication = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J1Technique
+        public double J1Suitability
         {
             get 
             { 
-                return this._j1technique; 
+                return this._j1suitability; 
             }
             set
             {
-                if (value != this._j1technique)
+                if (value != this._j1suitability)
                 {
-                    this._j1technique = value;
+                    this._j1suitability = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J1Execution
+        public double J1Composition
         {
             get
             {
-                return this._j1execution;
+                return this._j1composition;
             }
             set
             {
-                if (value != this._j1execution)
+                if (value != this._j1composition)
                 {
-                    this._j1execution = value;
+                    this._j1composition = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J1Artistry
+        public double J1Staging
         {
             get
             {
-                return this._j1artistry;
+                return this._j1staging;
             }
             set
             {
-                if (value != this._j1artistry)
+                if (value != this._j1staging)
                 {
-                    this._j1artistry = value;
+                    this._j1staging = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J1Showmanship
+        public double J1Difficulty
         {
             get
             {
-                return this._j1showmanship;
+                return this._j1difficulty;
             }
             set
             {
-                if (value != this._j1showmanship)
+                if (value != this._j1difficulty)
                 {
-                    this._j1showmanship = value;
+                    this._j1difficulty = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J1Appearance
+        public double J1Synchronization
         {
             get
             {
-                return this._j1appearance;
+                return this._j1synchronization;
             }
             set
             {
-                if (value != this._j1appearance)
+                if (value != this._j1synchronization)
                 {
-                    this._j1appearance = value;
+                    this._j1synchronization = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J1Spacing
+        {
+            get
+            {
+                return this._j1spacing;
+            }
+            set
+            {
+                if (value != this._j1spacing)
+                {
+                    this._j1spacing = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J1Movement
+        {
+            get
+            {
+                return this._j1movement;
+            }
+            set
+            {
+                if (value != this._j1movement)
+                {
+                    this._j1movement = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J1Dynamics
+        {
+            get
+            {
+                return this._j1dynamics;
+            }
+            set
+            {
+                if (value != this._j1dynamics)
+                {
+                    this._j1dynamics = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J1Elements
+        {
+            get
+            {
+                return this._j1elements;
+            }
+            set
+            {
+                if (value != this._j1elements)
+                {
+                    this._j1elements = value;
                     NotifyPropertyChanged();
                 }
             }
         }
 
         // Judge 2 Scores
-        public double J2Choreography
+        public double J2Communication
         {
             get
             {
-                return this._j2choreography;
+                return this._j2communication;
             }
             set
             {
-                if (value != this._j2choreography)
+                if (value != this._j2communication)
                 {
-                    this._j2choreography = value;
+                    this._j2communication = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J2Technique
+        public double J2Suitability
         {
             get
             {
-                return this._j2technique;
+                return this._j2suitability;
             }
             set
             {
-                if (value != this._j2technique)
+                if (value != this._j2suitability)
                 {
-                    this._j2technique = value;
+                    this._j2suitability = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J2Execution
+        public double J2Composition
         {
             get
             {
-                return this._j2execution;
+                return this._j2composition;
             }
             set
             {
-                if (value != this._j2execution)
+                if (value != this._j2composition)
                 {
-                    this._j2execution = value;
+                    this._j2composition = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J2Artistry
+        public double J2Staging
         {
             get
             {
-                return this._j2artistry;
+                return this._j2staging;
             }
             set
             {
-                if (value != this._j2artistry)
+                if (value != this._j2staging)
                 {
-                    this._j2artistry = value;
+                    this._j2staging = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J2Showmanship
+        public double J2Difficulty
         {
             get
             {
-                return this._j2showmanship;
+                return this._j2difficulty;
             }
             set
             {
-                if (value != this._j2showmanship)
+                if (value != this._j2difficulty)
                 {
-                    this._j2showmanship = value;
+                    this._j2difficulty = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J2Appearance
+        public double J2Synchronization
         {
             get
             {
-                return this._j2appearance;
+                return this._j2synchronization;
             }
             set
             {
-                if (value != this._j2appearance)
+                if (value != this._j2synchronization)
                 {
-                    this._j2appearance = value;
+                    this._j2synchronization = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J2Spacing
+        {
+            get
+            {
+                return this._j2spacing;
+            }
+            set
+            {
+                if (value != this._j2spacing)
+                {
+                    this._j2spacing = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J2Movement
+        {
+            get
+            {
+                return this._j2movement;
+            }
+            set
+            {
+                if (value != this._j2movement)
+                {
+                    this._j2movement = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J2Dynamics
+        {
+            get
+            {
+                return this._j2dynamics;
+            }
+            set
+            {
+                if (value != this._j2dynamics)
+                {
+                    this._j2dynamics = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J2Elements
+        {
+            get
+            {
+                return this._j2elements;
+            }
+            set
+            {
+                if (value != this._j2elements)
+                {
+                    this._j2elements = value;
                     NotifyPropertyChanged();
                 }
             }
         }
 
         // Judge 3 Scores
-        public double J3Choreography
+        public double J3Communication
         {
             get
             {
-                return this._j3choreography;
+                return this._j3communication;
             }
             set
             {
-                if (value != this._j3choreography)
+                if (value != this._j3communication)
                 {
-                    this._j3choreography = value;
+                    this._j3communication = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J3Technique
+        public double J3Suitability
         {
             get
             {
-                return this._j3technique;
+                return this._j3suitability;
             }
             set
             {
-                if (value != this._j3technique)
+                if (value != this._j3suitability)
                 {
-                    this._j3technique = value;
+                    this._j3suitability = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J3Execution
+        public double J3Composition
         {
             get
             {
-                return this._j3execution;
+                return this._j3composition;
             }
             set
             {
-                if (value != this._j3execution)
+                if (value != this._j3composition)
                 {
-                    this._j3execution = value;
+                    this._j3composition = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J3Artistry
+        public double J3Staging
         {
             get
             {
-                return this._j3artistry;
+                return this._j3staging;
             }
             set
             {
-                if (value != this._j3artistry)
+                if (value != this._j3staging)
                 {
-                    this._j3artistry = value;
+                    this._j3staging = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J3Showmanship
+        public double J3Difficulty
         {
             get
             {
-                return this._j3showmanship;
+                return this._j3difficulty;
             }
             set
             {
-                if (value != this._j3showmanship)
+                if (value != this._j3difficulty)
                 {
-                    this._j3showmanship = value;
+                    this._j3difficulty = value;
                     NotifyPropertyChanged();
                 }
             }
         }
-        public double J3Appearance
+        public double J3Synchronization
         {
             get
             {
-                return this._j3appearance;
+                return this._j3synchronization;
             }
             set
             {
-                if (value != this._j3appearance)
+                if (value != this._j3synchronization)
                 {
-                    this._j3appearance = value;
+                    this._j3synchronization = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J3Spacing
+        {
+            get
+            {
+                return this._j3spacing;
+            }
+            set
+            {
+                if (value != this._j3spacing)
+                {
+                    this._j3spacing = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J3Movement
+        {
+            get
+            {
+                return this._j3movement;
+            }
+            set
+            {
+                if (value != this._j3movement)
+                {
+                    this._j3movement = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J3Dynamics
+        {
+            get
+            {
+                return this._j3dynamics;
+            }
+            set
+            {
+                if (value != this._j3dynamics)
+                {
+                    this._j3dynamics = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public double J3Elements
+        {
+            get
+            {
+                return this._j3elements;
+            }
+            set
+            {
+                if (value != this._j3elements)
+                {
+                    this._j3elements = value;
                     NotifyPropertyChanged();
                 }
             }
